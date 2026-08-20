95e3e7401b42a79d63449000c49ff95c0fe2bfed
Zayed Ahmed <zayed967@yahoo.com>
2026-08-20 11:28:28 -0400

    correct path

--- src/Process.cpp
+++ src/Process.cpp
@@ -1,4 +1,4 @@
-#include "mini_supervisor/Process.hpp"
+#include "../include/mini_supervisor/Process.hpp"
 
 Process::Process(std::string procName, std::string procCommand, int restartLimit)
     : name(procName), command(procCommand), limit(restartLimit)
